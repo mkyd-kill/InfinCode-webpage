3652f085a90b3f04ebc44e39ff5537ca7a676f25
Romeo Namunde <romeomureithi@gmail.com>
2020-09-07 17:31:12 +0300

    My first web start

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+    /* Write a program that will send fullname, 
+    * email and query of the user as an email to the admin's email
+    * in php
+    */
+    
+    // Variable declaration
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+?>
